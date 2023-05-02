10b58098b35108d21306abf44c21776234449dcc
abhishek <abhirajabhi312@gmail.com>
2023-05-02 22:36:48 +0530

    add basic understanding of create element

--- learnings/script.js
+++ learnings/script.js
@@ -1,8 +1,8 @@
-const h2 = React.createElement(
-  "h2",
-  { className: "sub-head" },
-  "hello react you are awesome"
-);
+// const h2 = React.createElement(
+//   "h2",
+//   { className: "sub-head" },
+//   "hello react you are awesome"
+// );
 
 /*
 ReactDOM is a package in React that provides an interface for rendering React components to the DOM (Document Object Model), 
@@ -21,6 +21,58 @@ For example, here's how you might use ReactDOM.render() to render a simple React
 
 */
 
+// const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+// root.render(h2);
+
+const container = React.createElement(
+  "div",
+  { className: "container", id: "container" },
+  [
+    React.createElement("section", { key: 1 }, [
+      React.createElement(
+        "p",
+        { key: 1 },
+        "The library for web and native user interfaces"
+      ),
+      React.createElement("img", {
+        key: 2,
+        style: {
+          width: 200,
+          backgroundColor: "teal",
+          borderRadius: 8,
+          padding: 16,
+        },
+        src: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+      }),
+    ]),
+    React.createElement("section", { key: 2 }, [
+      React.createElement("form", { key: 1 }, [
+        React.createElement("div", { className: "input-group", key: 1 }, [
+          React.createElement(
+            "label",
+            { key: 1, htmlFor: "username" },
+            "Username"
+          ),
+          React.createElement("input", { key: 2, id: "username" }),
+        ]),
+        React.createElement("div", { className: "input-group", key: 2 }, [
+          React.createElement(
+            "label",
+            { key: 1, htmlFor: "password" },
+            "Password"
+          ),
+          React.createElement("input", {
+            key: 2,
+            id: "password",
+            type: "password",
+          }),
+        ]),
+      ]),
+    ]),
+  ]
+);
+
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
-root.render(h2);
+root.render(container);
